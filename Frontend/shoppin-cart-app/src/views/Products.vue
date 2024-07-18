562b432b631efa48fc26929724ddab3f79a6dd24
syed emad <syed.emad@idenfo.com>
2024-07-19 00:16:38 +0500

    feat: basic product item design

--- Frontend/shoppin-cart-app/src/views/Products.vue
+++ Frontend/shoppin-cart-app/src/views/Products.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Products</h1>
+  <h1 class="text-4xl font-bold">Products</h1>
+  <div class="w-full">
+    <div id="product-item" class="h-80 w-60">
+      <div
+        id="image-area"
+        class="h-3/4 flex items-center justify-center bg-gray-100 rounded-sm"
+      >
+        <img
+          src="../assets//weatherly_logo.png"
+          class="object-contain w-3/4 h-3/4"
+        />
+      </div>
+      <div id="detail-area" class="h-1/4 py-2">
+        <div class="flex justify-between">
+          <p class="text-sm font-medium text-gray-600">Shirt</p>
+          <p class="text-sm font-medium">30$</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
