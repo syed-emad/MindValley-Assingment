44b00326de8de605d1f8d39735665e69d772fbba
syed emad <syed.emad@idenfo.com>
2024-07-19 00:34:31 +0500

    feat: create Product Item component  and map dummy  data to it

---
+++ Frontend/shoppin-cart-app/src/components/ProductItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="product-item" class="h-80 w-full">
+    <div
+      id="image-area"
+      class="h-3/4 flex items-center justify-center bg-gray-100 rounded-sm"
+    >
+      <img :src="product.imageUrl" class="object-contain w-3/4 h-3/4" />
+    </div>
+    <div id="detail-area" class="h-1/4 py-2">
+      <div class="flex justify-between">
+        <p class="text-sm font-medium text-gray-600">{{ product.name }}</p>
+        <p class="text-sm font-medium">{{ product.price }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- Frontend/shoppin-cart-app/src/views/Products.vue
+++ Frontend/shoppin-cart-app/src/views/Products.vue
@@ -1,22 +1,39 @@
 <template>
   <h1 class="text-4xl font-bold">Products</h1>
-  <div class="w-full">
-    <div id="product-item" class="h-80 w-60">
-      <div
-        id="image-area"
-        class="h-3/4 flex items-center justify-center bg-gray-100 rounded-sm"
-      >
-        <img
-          src="../assets//weatherly_logo.png"
-          class="object-contain w-3/4 h-3/4"
-        />
-      </div>
-      <div id="detail-area" class="h-1/4 py-2">
-        <div class="flex justify-between">
-          <p class="text-sm font-medium text-gray-600">Shirt</p>
-          <p class="text-sm font-medium">30$</p>
-        </div>
-      </div>
-    </div>
+  <div
+    class="grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 bg-pink-900"
+  >
+    <product-item
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
   </div>
 </template>
+
+<script>
+import ProductItem from "../components/ProductItem.vue";
+export default {
+  components: {
+    ProductItem,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: "Shirt",
+          price: "300$",
+          imageUrl: "../assets/weatherly_logo.png",
+        },
+        {
+          id: 1,
+          name: "Shirt",
+          price: "300$",
+          imageUrl: "../assets/weatherly_logo.png",
+        },
+      ],
+    };
+  },
+};
+</script>
