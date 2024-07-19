0ff6970f2243ac6c86c96e32002965a8624b1457
syed emad <syed.emad@idenfo.com>
2024-07-20 02:54:44 +0500

    feat: integrate product list API, add basic route, improve CSS , add basic ease effect

--- Frontend/shoppin-cart-app/src/components/ProductItem.vue
+++ Frontend/shoppin-cart-app/src/components/ProductItem.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="product-item" class="h-80 w-full">
-    <div
-      id="image-area"
-      class="h-3/4 flex items-center justify-center bg-gray-100 rounded-sm"
-    >
-      <img :src="product.imageUrl" class="object-contain w-3/4 h-3/4" />
-    </div>
-    <div id="detail-area" class="h-1/4 py-2">
-      <div class="flex justify-between">
-        <p class="text-sm font-medium text-gray-600">{{ product.name }}</p>
-        <p class="text-sm font-medium">{{ product.price }}</p>
+  <router-link :to="'/product/' + product.id">
+    <div id="product-item" class="h-80 w-full">
+      <div
+        id="image-area"
+        class="h-3/4 flex items-center justify-center bg-gray-100 rounded-sm hover:scale-105 hover:opacity-80 cursor-pointer transition duration-300 ease-in-out"
+      >
+        <img :src="product.image_url" class="object-contain w-3/4 h-3/4" />
+      </div>
+      <div id="detail-area" class="h-1/4 py-2">
+        <div class="flex justify-between">
+          <p class="text-sm font-medium text-gray-600">{{ product.name }}</p>
+          <p class="text-sm font-medium">{{ product.price }}</p>
+        </div>
       </div>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <script>
--- Frontend/shoppin-cart-app/src/views/Products.vue
+++ Frontend/shoppin-cart-app/src/views/Products.vue
@@ -1,39 +1,40 @@
 <template>
-  <h1 class="text-4xl font-bold">Products</h1>
-  <div
-    class="grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 bg-pink-900"
-  >
-    <product-item
-      v-for="product in products"
-      :key="product.id"
-      :product="product"
-    />
+  <div class="space-y-10 px-10 py-5">
+    <h1 class="text-4xl font-bold">Products</h1>
+    <div
+      class="mt-5 grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5"
+    >
+      <product-item
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import ProductItem from "../components/ProductItem.vue";
+
 export default {
   components: {
     ProductItem,
   },
   data() {
     return {
-      products: [
-        {
-          id: 1,
-          name: "Shirt",
-          price: "300$",
-          imageUrl: "../assets/weatherly_logo.png",
-        },
-        {
-          id: 1,
-          name: "Shirt",
-          price: "300$",
-          imageUrl: "../assets/weatherly_logo.png",
-        },
-      ],
+      products: [],
     };
+  },
+  mounted() {
+    this.fetchProducts();
+  },
+  methods: {
+    fetchProducts() {
+      fetch("http://localhost:4000/api/products")
+        .then((res) => res.json())
+        .then((data) => (this.products = data.data))
+        .catch((err) => console.log(err.message));
+    },
   },
 };
 </script>
