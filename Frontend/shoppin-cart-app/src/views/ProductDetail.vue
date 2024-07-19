20890a341e18803ec70c0199be53cbacee9d8b05
syed emad <syed.emad@idenfo.com>
2024-07-20 03:30:39 +0500

    feat: product detail and add to cart button page

--- Frontend/shoppin-cart-app/src/views/ProductDetail.vue
+++ Frontend/shoppin-cart-app/src/views/ProductDetail.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>Product details</h1>
+  <div class="w-full flex lg:h-screen px-10">
+    <div class="w-1/2 flex items-center">
+      <div class="space-y-5">
+        <h1 class="text-3xl font-bold text-gray-900 sm:text-4xl">
+          Product Name Check
+        </h1>
+        <p class="text-lg text-gray-900 sm:text-xl">$200</p>
+        <p class="text-base text-gray-500 w-2/3">
+          Don't relinquish on transporting capacity with this featherweight and
+          capacious sack. The drawstring peak preserves all your preferred
+          crisps, fries, and biscuits secure.
+        </p>
+        <div class="w-3/4">
+          <button
+            type="submit"
+            class="flex w-full px-8 py-3 items-center justify-center rounded-md bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700"
+          >
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="w-1/2">
+      <img
+        src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+        class="w-full h-full object-contain"
+      />
+    </div>
+  </div>
 </template>
