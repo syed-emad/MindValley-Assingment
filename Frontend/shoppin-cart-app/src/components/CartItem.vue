cd43d7f3bba204b41287ea34924a2f73f83a754d
syed emad <syed.emad@idenfo.com>
2024-07-20 06:30:46 +0500

    feat: cart item component

---
+++ Frontend/shoppin-cart-app/src/components/CartItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-40 w-full flex bg-gray-50 rounded-lg p-5">
+    <div class="flex space-x-5">
+      <img
+        src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-02.jpg"
+        class="object-contain"
+      />
+      <div class="flex flex-col">
+        <p class="text-md font-medium text-gray-600">Basic Tee</p>
+        <p class="text-md font-medium">$32.00</p>
+      </div>
+    </div>
+  </div>
+</template>
