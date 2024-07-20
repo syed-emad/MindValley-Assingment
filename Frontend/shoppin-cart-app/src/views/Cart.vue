98f07d058fb97a6aba75601d57a0eeff5e22138b
syed emad <syed.emad@idenfo.com>
2024-07-20 16:54:31 +0500

    feat: integrate state getter in cart, add goback button

--- Frontend/shoppin-cart-app/src/views/Cart.vue
+++ Frontend/shoppin-cart-app/src/views/Cart.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="flex px-10 py-5 w-full justify-between items-center">
+    <img
+      @click="goBack()"
+      src="../assets/back.png"
+      class="h-10 w-10 cursor-pointer"
+    />
+  </div>
   <div class="px-10 space-y-5">
     <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
       Shopping Cart
@@ -14,13 +21,19 @@
           </h2>
           <div class="flex items-center justify-between">
             <p class="text-sm text-gray-600">Total</p>
-            <p class="text-sm font-medium text-gray-900">$99.00</p>
+            <p class="text-sm font-medium text-gray-900">${{ cartTotal }}</p>
+          </div>
+          <div class="flex items-center justify-between">
+            <p class="text-sm text-gray-600">Delivery</p>
+            <p class="text-sm font-medium text-gray-900">$0.00</p>
           </div>
           <div
             class="flex items-center justify-between border-t border-gray-200 pt-4"
           >
             <dt class="text-base font-medium text-gray-900">Order total</dt>
-            <dd class="text-base font-medium text-gray-900">$112.32</dd>
+            <dd class="text-base font-medium text-gray-900">
+              ${{ finalBill }}
+            </dd>
           </div>
           <button
             @click="
@@ -46,14 +59,29 @@
 </template>
 <script>
 import CartItem from "../components/CartItem.vue";
-import { mapState, mapActions, mapGetters } from "vuex";
+import { mapState, mapGetters } from "vuex";
 
 export default {
+  data() {
+    return {
+      finalBill: 0,
+      deliveryFee: 0,
+    };
+  },
+  mounted() {
+    this.finalBill = this.cartTotal + this.deliveryFee;
+  },
   components: {
     CartItem,
   },
   computed: {
     ...mapState(["cart"]),
+    ...mapGetters(["cartTotal"]),
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
   },
 };
 </script>
