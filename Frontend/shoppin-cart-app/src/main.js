77ec47f7277059da1fe2b99d25fe852c641eee5f
syed emad <syed.emad@idenfo.com>
2024-07-20 06:18:06 +0500

    feat: add vuex

--- Frontend/shoppin-cart-app/src/main.js
+++ Frontend/shoppin-cart-app/src/main.js
@@ -1,6 +1,47 @@
-import { createApp } from "vue";
+// import { createApp } from "vue";
 import "./style.css";
-import App from "./App.vue";
+// import App from "./App.vue";
 import router from "./routes/router";
+// import { createStore } from "vuex";
 
-createApp(App).use(router).mount("#app");
+// const store = createStore({
+//   state() {
+//     return {
+//       count: 0,
+//     };
+//   },
+//   mutations: {
+//     increment(state) {
+//       state.count++;
+//     },
+//   },
+// });
+// const app = createApp(App).use(router).mount("#app");
+// app.use(store);
+import { createApp } from "vue";
+import { createStore } from "vuex";
+import App from "./App.vue";
+// Create a new store instance.
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+      cart: [],
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+      console.log("state", this.count);
+    },
+    addToCart(state, product) {
+      state.cart.push(product);
+      console.log("state", this.cart);
+    },
+  },
+});
+
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount("#app");
