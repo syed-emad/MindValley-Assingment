1fa52355102c412d34315d64ce866f74c5eaafbc
syed emad <syed.emad@idenfo.com>
2024-07-20 06:30:28 +0500

    feat: fetch data from API

--- Frontend/shoppin-cart-app/src/views/ProductDetail.vue
+++ Frontend/shoppin-cart-app/src/views/ProductDetail.vue
@@ -13,6 +13,17 @@
         </p>
         <div class="w-3/4">
           <button
+            @click="
+              addToCart({
+                id: 3,
+                name: 'Product A',
+                description: 'hello',
+                image_url:
+                  'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+                price: '10.99',
+                quantity: 1,
+              })
+            "
             type="submit"
             class="flex w-full px-8 py-3 items-center justify-center rounded-md bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700"
           >
@@ -29,3 +40,28 @@
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      product: {},
+    };
+  },
+  mounted() {
+    this.fetchProduct();
+  },
+  methods: {
+    addToCart(product) {
+      this.$store.commit("addToCart", product);
+      console.log("asd", this.$store.state.cart);
+    },
+    fetchProduct() {
+      console.log(this.$route.params.id);
+      fetch(`http://localhost:4000/api/product/${this.$route.params.id}`)
+        .then((res) => res.json())
+        .then((data) => (this.product = data.data))
+        .catch((err) => console.log(err.message));
+    },
+  },
+};
+</script>
