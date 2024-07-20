5b3392baef5fc5f0a841e735b2879e09c01ecef4
syed emad <syed.emad@idenfo.com>
2024-07-20 06:13:13 +0500

    refactor: update detail route feat: create Cart route

--- Frontend/shoppin-cart-app/src/routes/router.js
+++ Frontend/shoppin-cart-app/src/routes/router.js
@@ -2,9 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   { path: "", name: "home", component: () => import("../views/Products.vue") },
   {
-    path: "/detail",
+    path: "/detail/:id",
     name: "product",
     component: () => import("../views/ProductDetail.vue"),
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: () => import("../views/Cart.vue"),
   },
 ];
 const router = createRouter({
